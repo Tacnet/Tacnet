522fd32831fcf81b5aa024b92214d37d854b0dc5
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-10-22 23:36:48 +0200

    Add block to index, about, contact

--- tacnet/apps/frontpage/urls.py
+++ tacnet/apps/frontpage/urls.py
@@ -4,4 +4,8 @@ urlpatterns = patterns('frontpage',
     # url(r'positionlock/(?P<position>\d+)/set/$', 'views.set_position_lock', name="set_position_lock"),
     # url(r'^index/$', 'views.index', name="index"),
     url(r'^$', 'views.index', name="index"),
+
+    url(r'^about/$', 'views.about', name="about"),
+    url(r'^contact/$', 'views.contact', name="contact"),
+
 )
