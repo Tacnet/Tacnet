824f7e4b39feaf4a396b5d566a999ed6bdedf867
Martin Ek <ekmartinb@gmail.com>
2013-10-22 23:20:49 +0200

    frontpage --> tacsketch in urls.py

--- tacnet/apps/tacsketch/urls.py
+++ tacnet/apps/tacsketch/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import *
 
-urlpatterns = patterns('frontpage',
+urlpatterns = patterns('tacsketch',
     # url(r'positionlock/(?P<position>\d+)/set/$', 'views.set_position_lock', name="set_position_lock"),
     # url(r'^index/$', 'views.index', name="index"),
     url(r'^$', 'views.index', name="index"),
--- tacnet/tacnet/urls.py
+++ tacnet/tacnet/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
     #(r'^$', 'errors.views.underdevelopment'),
 
     (r'^frontpage/', include('frontpage.urls')),
-    (r'^frontpage/', include('tacsketch.urls'))
+    (r'^tacsketch/', include('tacsketch.urls'))
 
 )
 urlpatterns += staticfiles_urlpatterns()
