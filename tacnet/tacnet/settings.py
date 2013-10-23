502a28696e53af91b152bda37e1542b29e177604
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-10-23 17:11:50 +0200

    Added collectstatic in deploy script

--- tacnet/tacnet/settings.py
+++ tacnet/tacnet/settings.py
@@ -80,8 +80,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     makepath('apps', 'base', 'static'),
-    makepath('apps', 'frontpage', 'static'),
-    makepath('apps', 'tacsketch', 'static')
+    makepath('apps', 'tacsketch', 'static'),
 )
 
 # List of finder classes that know how to find static files in
