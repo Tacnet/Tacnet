874516df5fab73161dd1fcec292810b6b984eeec
Martin Ek <ekmartinb@gmail.com>
2013-10-28 03:00:56 +0100

    Add TogetherJS config settings.

---
+++ tacnet/apps/tacsketch/static/js/TogetherJSConfig.js
@@ -0,0 +1,5 @@
+var TogetherJSConfig_autoStart = true;
+var TogetherJSConfig_suppressJoinConfirmation = true;
+var TogetherJSConfig_siteName = "Tacnet";
+var TogetherJSConfig_toolName = "Tacnet";
+var TogetherJSConfig_disableWebRTC = true;
