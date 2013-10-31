0bf1657b040bebc601842db2cd8e82e0a4174e31
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-10-31 21:21:36 +0100

    New FRESH admin

--- tacnet/tacnet/urls.py
+++ tacnet/tacnet/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 # Custom 404 file
 handler404 = 'errors.views.error404'
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     (r'^$', 'frontpage.views.index'),
     #(r'^$', 'errors.views.underdevelopment'),
