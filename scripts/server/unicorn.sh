5b816a8a1a5e126ee3efcd457417287cda0ab593
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-11-01 18:01:07 +0100

    Added google analytics and newrelic

--- scripts/server/unicorn.sh
+++ scripts/server/unicorn.sh
@@ -9,10 +9,13 @@ ADDRESS=127.0.0.1:9002
 cd /home/tacnet-www/www/tacnet/
 source /home/tacnet-www/www/venv/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDI
-#export NEW_RELIC_CONFIG_FILE=/home/tacnet-www/www/scripts/server/newrelic.ini
-#exec newrelic-admin run-program gunicorn tacnet.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
-#  --user=$USER --group=$GROUP --log-level=debug \
-#  --log-file=$LOGFILE 2>>$LOGFILE
-exec gunicorn tacnet.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
+
+export NEW_RELIC_CONFIG_FILE=/home/tacnet-www/www/scripts/server/newrelic.ini
+exec newrelic-admin run-program gunicorn tacnet.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
   --user=$USER --group=$GROUP --log-level=debug \
   --log-file=$LOGFILE 2>>$LOGFILE
+
+
+#exec gunicorn tacnet.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
+#  --user=$USER --group=$GROUP --log-level=debug \
+#  --log-file=$LOGFILE 2>>$LOGFILE
