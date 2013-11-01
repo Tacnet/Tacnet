252ba154f10e414b5ac918ac525cbdf325db64dd
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-11-01 23:50:41 +0100

    Postgresql server

--- tacnet/tacnet/settings-production.py
+++ tacnet/tacnet/settings-production.py
@@ -1,1 +1,16 @@
 DEBUG = False
+
+with open('/home/tacnet-www/www/passwords/tacnet_db', 'rb') as f:
+    db_password = f.readline()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tacnet_db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'tacnet',
+        'PASSWORD': db_password.strip(),
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
