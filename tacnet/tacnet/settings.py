032c986a68ff7cbae7e17dd3da4bca075847c08b
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-11-01 18:06:03 +0100

    Added ip to allowed hosts

--- tacnet/tacnet/settings.py
+++ tacnet/tacnet/settings.py
@@ -30,13 +30,13 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['ek.sylliaas.no']
+ALLOWED_HOSTS = ['ek.sylliaas.no', '129.241.208.163']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Oslo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
