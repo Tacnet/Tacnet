deba8353a76cbbc0c9f5b3898505bd7a91444f8c
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2013-11-03 17:02:27 +0100

    Added www.tacnet.io to allowed_hosts

--- tacnet/tacnet/settings.py
+++ tacnet/tacnet/settings.py
@@ -31,7 +31,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['ek.sylliaas.no', '129.241.208.163', 'tacnet.io']
+ALLOWED_HOSTS = ['ek.sylliaas.no', '129.241.208.163', 'tacnet.io', 'www.tacnet.io']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
