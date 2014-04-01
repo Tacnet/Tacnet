2f0ae14fb5a9126c910f9d775ddcc6a49385f391
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-04-01 15:19:09 +0200

    Change mail

--- system/settings/production.py
+++ system/settings/production.py
@@ -40,3 +40,9 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
 EMAIL_USE_TLS = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_SUBJECT_PREFIX = "[Tacnet] "
+SERVER_EMAIL = "Tacnet" + ' <' + "tacnet@tacnet.io" + '>'
+DEFAULT_FROM_EMAIL = SERVER_EMAIL
